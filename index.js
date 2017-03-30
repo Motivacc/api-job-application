1281ded0f5b74da7b3a07123d113c7eefd62499a
Antonio Nicasio <webmediadigital@gmail.com>
2017-03-30 13:51:58 -0700

    cambios y seteo de los headers en el servidor

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
+// Cross Origin Middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 // sesteamos nuestras API Routes
 app.use('/', api);
 
